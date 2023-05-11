89db22aff91f253d54bb3f7953758d9aead4a7c3
scott mcgann <s.r.mcgann@hotmail.com>
2023-05-11 21:39:13 +0000

    sync

--- public_playlists/temp/run.php
+++ public_playlists/temp/run.php
@@ -1,10 +1,4 @@
 <?php
-//chdir("public_playlists/temp");
-echo shell_exec("wget https://www.dropbox.com/s/jxxdqvyfjsbc7a3/temp.zip.001?raw=1 -O temp.zip.001") . "\n";
-echo shell_exec("wget https://www.dropbox.com/s/07gzev10vtctk6g/temp.zip.002?raw=1 -O temp.zip.002") . "\n";
-echo shell_exec("wget https://www.dropbox.com/s/p588lmnqxnyfp3f/temp.zip.003?raw=1 -O temp.zip.003") . "\n";
-echo shell_exec("wget https://www.dropbox.com/s/3ys4t6jckwep3fo/temp.zip.004?raw=1 -O temp.zip.004") . "\n";
-echo shell_exec("wget https://www.dropbox.com/s/wu0t9niyz8d178q/temp.zip.005?raw=1 -O temp.zip.005") . "\n\n\n";
 echo "...backgrounding zip operation.. \n\n";
 echo shell_exec("screen -d -m ./run.sh") . "\n";
 ?>
--- public_playlists/temp/run.sh
+++ public_playlists/temp/run.sh
@@ -1,4 +1,9 @@
 !/bin/bash
+wget https://www.dropbox.com/s/jxxdqvyfjsbc7a3/temp.zip.001?raw=1 -O temp.zip.001
+wget https://www.dropbox.com/s/07gzev10vtctk6g/temp.zip.002?raw=1 -O temp.zip.002
+wget https://www.dropbox.com/s/p588lmnqxnyfp3f/temp.zip.003?raw=1 -O temp.zip.003
+wget https://www.dropbox.com/s/3ys4t6jckwep3fo/temp.zip.004?raw=1 -O temp.zip.004
+wget https://www.dropbox.com/s/wu0t9niyz8d178q/temp.zip.005?raw=1 -O temp.zip.005
 cat temp.zip.001 > file.zip
 cat temp.zip.002 >> file.zip
 cat temp.zip.003 >> file.zip
